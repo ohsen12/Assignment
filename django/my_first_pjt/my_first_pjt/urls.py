40d6d431938a3bd504d5fd1df75c8824061abcd0
SEEUN OH <tpsdms121@gmail.com>
2025-01-02 11:10:45 +0900

    프로젝트 구성 파일 설명 주석 추가

--- django/my_first_pjt/my_first_pjt/urls.py
+++ django/my_first_pjt/my_first_pjt/urls.py
@@ -1,3 +1,6 @@
+# urls.py 파일은 Django 프로젝트에서 URL 라우팅을 관리하는 파일입니다. 이 파일을 통해 특정 URL이 요청되었을 때 어떤 뷰(View) 함수가 호출될지를 결정합니다.
+# URL 라우팅(URL Routing)은 웹 애플리케이션에서 요청된 URL을 특정한 코드나 함수로 연결하는 과정을 의미해요. 쉽게 말해, 사용자가 웹 브라우저에 특정 URL을 입력했을 때 그 URL에 해당하는 웹 페이지나 기능을 제공할 수 있도록 하는 작업입니다.
+
 """
 URL configuration for my_first_pjt project.
 
--- django/my_first_pjt/my_first_pjt/wsgi.py
+++ django/my_first_pjt/my_first_pjt/wsgi.py
@@ -1,3 +1,6 @@
+# wsgi.py 파일은 Django 프로젝트에서 웹 서버 게이트웨이 인터페이스(WSGI)를 설정하는 파일입니다. 
+# WSGI는 Django가 동기 웹 서버 및 애플리케이션과 통신할 수 있도록 하는 표준 인터페이스입니다.
+
 """
 WSGI config for my_first_pjt project.
 
