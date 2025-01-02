5c21bbbd80e7f076174b05404fb9626128b4957f
SEEUN OH <tpsdms121@gmail.com>
2025-01-02 20:29:05 +0900

    data-catch 수정

--- django/my_first_pjt/articles/views.py
+++ django/my_first_pjt/articles/views.py
@@ -35,4 +35,18 @@ def data_throw(request):
     return render(request,'data_throw.html')
 
 def data_catch(request):
-    return render(request,'data_catch.html')
+    '''
+    data-throw 에서 입력한 데이터를 message라는 name(key)으로 값을 전달해줬기 때문에
+    뷰에서 message라는 key에 있는 값을 꺼내라는 것이다.
+    그럼 그 값을 context에 담아 render 함수의 세 번째 인자로 넘겨준다.
+    
+    ⭐️ render 함수에서 context는 '템플릿에 전달할 데이터를 포함'하는 역할을 한다. 
+    context는 딕셔너리 형태로 작성되며, 템플릿에서 사용될 변수를 키-값 쌍으로 정의한다. 
+    이를 통해 템플릿은 동적 데이터를 포함하여 HTML 페이지를 생성할 수 있다.
+    
+    템플릿에서 contex 딕셔너리에 정의된 변수들을 사용하여 HTML 페이지를 만들 수 있다는 것이다. 
+    근데 이때 context를 세 번째 인자로 전달하지 않으면 템플릿에서 변수를 사용할 수 없어서, HTML 출력에 동적 데이터가 포함되지 않게 되니 주의해야 한다!  
+    '''
+    message = request.GET.get("message") # 서버로 전송된 message라는 키에 있는 값이 담김
+    context = {"message": message}
+    return render(request,'data_catch.html',context)
