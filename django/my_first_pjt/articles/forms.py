c9f44af6d03b104f32579d2c99502ad87db4f65a
SEEUN OH <tpsdms121@gmail.com>
2025-01-07 16:28:19 +0900

    전체 댓글 로직 작성완료

--- django/my_first_pjt/articles/forms.py
+++ django/my_first_pjt/articles/forms.py
@@ -3,9 +3,9 @@
 # 이를 통해 Django 기본 폼 기능을 확장하거나 커스터마이징할 수 있다.
 
 from django import forms
-from articles.models import Article
+from articles.models import Article,Comment
 
-# 모델을 보고 
+# 게시글(Article) 폼 정의
 class ArticleForm(forms.ModelForm):
     # ModelForm이 사용할 데이터를 Meta 클래스에 명시
     class Meta:
@@ -14,6 +14,17 @@ class ArticleForm(forms.ModelForm):
         # fields 항목에 내가 form으로 만들고 싶은 항목들을 지정 (__all__은 모델의 모든 필드를 입력하게 하겠다.)
         fields = "__all__"
         # exclude = ["굳이 입력 안 받아도 되는 컬럼"]
+        
+
+# 댓글(Comment) 폼 정의
+class CommentForm(forms.ModelForm):
+    class Meta:
+        # Comment 모델을 사용해서 form을 만들거임
+        model = Comment
+        # 모델에서 보여줄 필드 정의
+        fields = "__all__"
+        # 댓글 작성할 때 게시글은 사용자가 고르는 게 아니라 그냥 해당 아티클에 다는 거니까 아티클 필드는 보여주지 마
+        exclude = ("article",)
 
     
     
