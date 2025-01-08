9ddd56db2626cf7513f52efc9c91126378d935cf
SEEUN OH <tpsdms121@gmail.com>
2025-01-08 22:30:15 +0900

    좋아요 기능까지 구현

--- django/my_first_pjt/articles/models.py
+++ django/my_first_pjt/articles/models.py
@@ -17,10 +17,16 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True) # auto_now_add=True : 새로운 행(레코드)가 '생성될 때'의 날짜와 시간을 자동으로 설정한다. 이후에 레코드가 업데이트되더라도 이 필드는 변경되지 않고 레코드가 처음 생성된 시점을 나타낸다.
     updated_at = models.DateTimeField(auto_now=True) # 행(레코드)이 생성되거나 '저장될 때마다' 해당 필드에 현재 날짜와 시간이 자동으로 설정됨. 즉, 레코드가 변경될 때마다 이 필드는 업데이트된다.
     image = models.ImageField(upload_to="images/", blank = True) # blank=True면 폼에서 해당 값을 입력하지 않아도 된다.
+    
     # 장고 설정에서 유저 모델(테이블)을 참조해라. 게시글이 어떤 유저랑 연결되어 있는지를 나타낸다.
     # 게시글이 어느 사용자가 작성했는지를 나타내는 컬럼.(화면에 표시되는 필드는 아님)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="articles")
     
+    # 다대다(M:N) 관계 설정시 사용하는 모델 필드. 중계 테이블은 내가 커스텀해서 만든 모델로 해줄거임. 이렇게 하면 장고가 만들어주는 그 테이블이 생성되지 않음. (❗️ 이거는 article 테이블에 존재하는 컬럼이 아님! 장고가 자동으로 생성한 중간 테이블에서 관리됨!!)
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="like_articles"
+    )
+
     def __str__(self): # 클래스를 문자열 취급했을 때 어떻게 보여줄 지 결정하는 매직메서드
         return self.title
     
@@ -29,7 +35,7 @@ class Article(models.Model):
 # 각 댓글이 어떤 게시글에 달려있는지 알아야 하기 때문에 외래키를 이용해 Article 모델(테이블)을 참조함. 
 class Comment(models.Model):
     # 얘는 화면에 직접 표시되는 필드가 아니라, 데이터베이스에서 댓글(Comment)과 게시글(Article) 간의 관계를 정의하는 컬럼이다.
-    # 댓글 모델의 외래키 필드는 백엔드에서 데이터베이스의 관계를 설정하고, 댓글이 어떤 게시글에 속해 있는지를 나타낸다.
+    # 댓글 모델의 외래키 필드는 댓글이 어떤 게시글에 속해 있는지를 나타내는 컬럼이다.
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="comments") # related_name은 아티클에서 댓글을 역참조 할 때 사용하는 매니저이다.
     
     # 댓글의 작성자가 누구인지 유저 테이블을 참조
@@ -42,3 +48,14 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.content
+    
+
+# 좋아요 모델(좋아요를 어떤 글에 누가 눌렀는지 보여주는 테이블)
+class ArticleLike(models.Model):
+    # 어떤 아티클인지 참조하는 외래키 (아티클 모델과의 관계 정의)
+    # CASCADE:참조된 게시글이 삭제되면 해당 게시글과 관련된 좋아요도 함꼐 삭제된다.
+    # realated_name: Article 모델에서 역참조할 때 사용하는 매니저 이름. 예를 들어, 특정 게시글에 달린 모든 '좋아요'를 가져오려면 article.likes.all()을 사용할 수 있다.
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="likes")
+    # 어떤 사용자인지 참조하는 외래키. Django 설정에서 정의된 사용자 모델을 참조한다.
+    # 특정 사용자가 눌렀던 모든 '좋아요'를 가져오려면 user.likes.all()을 사용할 수 있다.
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="likes")
--- django/my_first_pjt/articles/views.py
+++ django/my_first_pjt/articles/views.py
@@ -20,12 +20,41 @@ def index(request):
     return render(request,'articles/index.html')
 
 
+def data_throw(request):
+    return render(request,'articles/data_throw.html')
+
+
+def data_catch(request):
+    '''
+    data-throw 에서 입력한 데이터를 message라는 name(key)으로 값을 전달해줬기 때문에
+    뷰에서 message라는 key에 있는 값을 꺼내라는 것이다.
+    그럼 그 값을 context에 담아 render 함수의 세 번째 인자로 넘겨준다.
+    
+    ⭐️ render 함수에서 context는 '템플릿에 전달할 데이터를 포함'하는 역할을 한다. 
+    context는 딕셔너리 형태로 작성되며, 템플릿에서 사용될 변수를 키-값 쌍으로 정의한다. 
+    이를 통해 템플릿은 동적 데이터를 포함하여 HTML 페이지를 생성할 수 있다.
+    
+    템플릿에서 contex 딕셔너리에 정의된 변수들을 사용하여 HTML 페이지를 만들 수 있다는 것이다. 
+    근데 이때 context를 세 번째 인자로 전달하지 않으면 템플릿에서 변수를 사용할 수 없어서, HTML 출력에 동적 데이터가 포함되지 않게 되니 주의해야 한다!  
+    '''
+    message = request.GET.get("message") # 서버로 데이터가 message라는 키값으로 전송되니까 딕셔너리의 get 메서드를 사용해서 해당 키의 값을 꺼내오는 것임
+    context = {"message": message}
+    return render(request,'articles/data_catch.html',context)
+
+
 # 데이터베이스에서 Article 테이블을 전부 가져와서 화면에 보여주는 로직
 def articles(request): 
+    # 모든 게시글 들고와
     articles = Article.objects.all().order_by("-id") # id(pk)을 기준으로 내림차순 정렬
     
+    # ⭐️ Python에서는 동적으로 객체에 속성을 추가할 수 있다.
+    # 비록 Article 모델에 like_count 속성이 기본적으로 정의되어 있지 않지만, 런타임 중에 동적으로 추가할 수 있다.
+    # 이렇게 하면 like_count 속성은 데이터베이스에 저장되지 않지만, 뷰에서 템플릿으로 전달되는 객체에서는 사용할 수 있다!!
+    for article in articles: 
+        article.like_count = article.like_users.count()
+    
     context = {
-        "articles" : articles
+        "articles" : articles,
     }
     return render(request,'articles/articles.html',context)
 
@@ -53,8 +82,8 @@ def create(request):
     if request.method == "POST": # 새글 작성하고 저장 누른 거임. 이제 데이터베이스에 전송받은 글 저장해야지.
         form = ArticleForm(request.POST, request.FILES) # form에 request.POST에 있는(전송받은) 데이터 채워
         if form.is_valid(): # form 형식에 맞으면
-            article = form.save(commit=False)
-            article.author = request.user # 지금 로그인한 유저
+            article = form.save(commit=False) # 바로 냅다 저장할 수 없어서 commit=False 로 지정. 필요한 속성(컬럼)을 다 채워야 함!
+            article.author = request.user # 해당 객체에 author 속성 채워야 되는데, 그거를 지금 요청 보낸 유저로 채워. (현재 로그인한 사람)
             article.save()
             return redirect("articles:article_detail", article.pk) # 저장된 해당 글 상세페이지로 넘어가기
     else: # 새로운 아티클 작성하러 가기 앵커 태그(GET방식) 누르고 새 글 작성하러 온 거임
@@ -63,7 +92,7 @@ def create(request):
     return render(request, "articles/create.html", context) # 이제 해당 페이지에서 submit 버튼 누르면 POST 방식으로 데이터를 담아 다시 이 뷰에 전송함.
 
 
-@login_required # # 로그인이 되어있지 않은 상태에서 접근하면 settings.LOGIN_URL 에 설정된 경로(기본은 로그인 페이지)로 이동시킴
+@login_required # 로그인이 되어있지 않은 상태에서 접근하면 settings.LOGIN_URL 에 설정된 경로(기본은 로그인 페이지)로 이동시킴
 @require_http_methods(["GET", "POST"]) # 요청이 이 방식일 때만 처리
 def update(request, pk):
     # 일단 해당 글 가져와서 객체에 넣어놔
@@ -110,11 +139,11 @@ def comment_create(request, pk):
         # CommentForm의 데이터를 이용해 Comment 객체를 생성하되, 아직 데이터베이스에 저장하지 않는다. commit=False 옵션을 사용하여 인스턴스만 생성함
         # comment.article과 같이 폼에는 포함되지 않은 데이터를 설정할 필요가 있다. 이렇게 해야 Comment가 특정 Article과 연결된다.
         comment = form.save(commit=False)
-        # 새로 생성된 댓글 객체(comment)의 article 속성을 위에서 가져온 Article 객체로 설정한다. 이를 통해 댓글이 어느 게시글에 속하는지를 지정한다!
+        # 새로 생성된 댓글 객체(comment)의 article 속성을 위에서 가져온 Article 객체로 설정한다. 이를 통해 해당 댓글이 어느 게시글에 속하는지를 지정한다!
         comment.article = article
-        # 새로 생성된 댓글 객체의 user 속성을 요청을 넘긴 유저로 설정한다.
+        # 새로 생성된 댓글 객체의 user 속성을 요청을 넘긴(댓글 입력하고 작성버튼을 누른) 유저로 설정한다.
         comment.user = request.user
-        # 이제 댓글 객체를 데이터베이스에 저장한다.
+        # 이제 필요한 모든 컬럼을 채웠으니 댓글 객체를 데이터베이스에 저장한다.
         comment.save()
     return redirect("articles:article_detail", article.pk)
 
@@ -131,23 +160,25 @@ def comment_delete(request, pk, comment_pk):
     return redirect("articles:article_detail", pk)
 
 
-def data_throw(request):
-    return render(request,'articles/data_throw.html')
+@require_POST
+# 좋아요(or 좋아요 취소) 버튼 누르고 이 뷰로 넘어왔음
+def like(request, pk):
+    # 현재 사용자가 로그인되어 있다면
+    if request.user.is_authenticated:
+        # 해당 게시글 들고오고
+        article = get_object_or_404(Article, pk=pk)
+        # 해당 게시글에 좋아요 누른 사람 중에 요청 보낸 회원의 pk가 존재한다면 (해당 사용자가 현재 좋아요를 이미 누른 상태라는 거니까. 이 사람은 좋아요 취소 버튼 누르고 온거임)
+        if article.like_users.filter(pk=request.user.pk).exists():
+            print("22")
+            # 해당 게시글 좋아요 누른 사람에서 해당 회원 없애기
+            article.like_users.remove(request.user)
+        # 아직 해당 게시글에 좋아요를 누르지 않은 회원이라면(좋아요 버튼 눌렀다는 거니까)
+        
+        else:
+            print("11")
+            # 해당 게시글 좋아요 누른 사람에 해당 회원 추가하기
+            article.like_users.add(request.user)
+        return redirect("articles:articles")
+    # 현재 사용자가 로그인되어 있지 않는데 따봉 버튼을 눌렀다면 로그인 페이지로 보내기 (로그인해야 사용할 수 있는 기능이에요~)
+    return redirect("accounts:login")
 
-
-def data_catch(request):
-    '''
-    data-throw 에서 입력한 데이터를 message라는 name(key)으로 값을 전달해줬기 때문에
-    뷰에서 message라는 key에 있는 값을 꺼내라는 것이다.
-    그럼 그 값을 context에 담아 render 함수의 세 번째 인자로 넘겨준다.
-    
-    ⭐️ render 함수에서 context는 '템플릿에 전달할 데이터를 포함'하는 역할을 한다. 
-    context는 딕셔너리 형태로 작성되며, 템플릿에서 사용될 변수를 키-값 쌍으로 정의한다. 
-    이를 통해 템플릿은 동적 데이터를 포함하여 HTML 페이지를 생성할 수 있다.
-    
-    템플릿에서 contex 딕셔너리에 정의된 변수들을 사용하여 HTML 페이지를 만들 수 있다는 것이다. 
-    근데 이때 context를 세 번째 인자로 전달하지 않으면 템플릿에서 변수를 사용할 수 없어서, HTML 출력에 동적 데이터가 포함되지 않게 되니 주의해야 한다!  
-    '''
-    message = request.GET.get("message") # 서버로 데이터가 message라는 키값으로 전송되니까 딕셔너리의 get 메서드를 사용해서 해당 키의 값을 꺼내오는 것임
-    context = {"message": message}
-    return render(request,'articles/data_catch.html',context)
