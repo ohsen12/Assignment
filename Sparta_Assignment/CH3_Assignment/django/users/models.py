61c2306089d56414595dc6be14710b762e2396bf
SEEUN OH <tpsdms121@gmail.com>
2025-01-14 15:39:10 +0900

    초기 앱 생성 및 마이그레이션 완료

--- Sparta_Assignment/CH3_Assignment/django/users/models.py
+++ Sparta_Assignment/CH3_Assignment/django/users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+# 커스텀 유저모델 정의
 class CustomUser(AbstractUser):
-    # 이미지 업로드 필드 (blank = True 옵션을 줘서 이 부분 없이 업로드하는 것도 가능하게 함)
-    profile_image = models.ImageField(upload_to="images/", blank = True) 
-    introduction = models.TextField(blank = True)
+    profile_image = models.ImageField(upload_to="images/", blank = True)
+    introduction = models.TextField(blank=True)
