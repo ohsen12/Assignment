871cce61fe5f0696231bf6fc1cc57531d6f2fb13
SEEUN OH <tpsdms121@gmail.com>
2025-01-21 16:24:05 +0900

    users 앱에 커스텀 유저 모델 정의 후 세팅 파일에 설정

--- Sparta_Assignment/CH3_Assignment/2. drf/my_pjt/users/models.py
+++ Sparta_Assignment/CH3_Assignment/2. drf/my_pjt/users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# 커스텀 유저모델 정의
+class CustomUser(AbstractUser):
+    # upload_to 는 MEDIA_ROOT의 하위 경로를 지정한다. 즉, 이미지가 업로드 되는 경로
+    profile_image = models.ImageField(upload_to="images/", blank = True)
+    introduction = models.TextField(blank=True)
