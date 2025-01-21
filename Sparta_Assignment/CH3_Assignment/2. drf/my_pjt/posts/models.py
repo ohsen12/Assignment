75cd6b0c0a826770c6fc74d726017b8c59ad4498
SEEUN OH <tpsdms121@gmail.com>
2025-01-21 19:49:57 +0900

    posts 앱 Post 모델 만들고 마이그레이션

--- Sparta_Assignment/CH3_Assignment/2. drf/my_pjt/posts/models.py
+++ Sparta_Assignment/CH3_Assignment/2. drf/my_pjt/posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+# 게시글 모델(테이블)
+class Post(models.Model):
+    # ⭐️ 게시글을 누가 작성한 것인지 연결해주기 위해 외래키 사용
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts")
+    # 제목
+    title = models.CharField(max_length=50)
+    # 내용
+    content = models.TextField()
+    # auto_now_add=True : 새로운 행(레코드)가 '생성될 때'의 날짜와 시간을 자동으로 설정한다.
+    created_at = models.DateTimeField(auto_now_add=True) 
+    # auto_now=True : 행(레코드)이 생성되거나 '저장될 때마다' 해당 필드에 현재 날짜와 시간이 자동으로 설정됨. 즉, 레코드가 변경될 때마다 이 필드는 업데이트된다.
+    updated_at = models.DateTimeField(auto_now=True)
--- Sparta_Assignment/CH3_Assignment/2. drf/my_pjt/posts/urls.py
+++ Sparta_Assignment/CH3_Assignment/2. drf/my_pjt/posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-
+from . import views
 
 urlpatterns = [
-    
+    # Create
+    path("post_create/", views.post_create, name="post_create"),
 ]
